c13ee5e8996f86afbdd7fb1af79e2b76d31e70d6
armanschar <ascharbay@hotmail.com>
2025-08-18 20:08:04 -0400

    actualizacion manejo de sesiones mongo fix

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     store: MongoStore.create({
-      mongoUrl: process.env.MONGODB_URI,
+      mongoUrl: process.env.MONGO_URI,
       collectionName: "sessions",
       ttl: 14 * 24 * 60 * 60, // 14 days
     }),
